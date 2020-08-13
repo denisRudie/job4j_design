9265780dcd225b0cca3f26b84d5418d901d6d32e
denisRudie <moonstamp1964@gmail.com>
2020-08-13 20:28:38 +0300

    3.0. Тестирование IO [#321406]

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,24 +1,39 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder temp = new TemporaryFolder();
+
     @Test
-    public void whenHave2PeriodsOfUnavailable() {
+    public void whenHave2PeriodsOfUnavailable() throws IOException {
+        File source = temp.newFile();
+        File target = temp.newFile();
         Analizy analizy = new Analizy();
-        analizy.unavailable("data/log.txt", "data/unavailable.csv");
-        try (BufferedReader reader = new BufferedReader(new FileReader("data/unavailable.csv"))){
+
+        try (PrintWriter pw = new PrintWriter(new FileWriter(source))) {
+            pw.println("200 10:56:01\n" +
+                    "500 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "500 11:01:02\n" +
+                    "200 11:02:02");
+        }
+
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))){
             assertThat(reader.readLine(), is("10:57:01;10:59:01"));
             assertThat(reader.readLine(), is("11:01:02;11:02:02"));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
