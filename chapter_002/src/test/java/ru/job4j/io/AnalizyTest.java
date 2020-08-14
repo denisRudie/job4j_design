ee0bd5bb76b9ddbb1e34062f02d3d9f076de469b
denisRudie <moonstamp1964@gmail.com>
2020-08-14 11:13:06 +0300

    3.0. Тестирование IO [#321406] - refactoring

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -21,17 +21,17 @@ public class AnalizyTest {
         Analizy analizy = new Analizy();
 
         try (PrintWriter pw = new PrintWriter(new FileWriter(source))) {
-            pw.println("200 10:56:01\n" +
-                    "500 10:57:01\n" +
-                    "400 10:58:01\n" +
-                    "200 10:59:01\n" +
-                    "500 11:01:02\n" +
-                    "200 11:02:02");
+            pw.println("200 10:56:01");
+            pw.println("500 10:57:01");
+            pw.println("400 10:58:01");
+            pw.println("200 10:59:01");
+            pw.println("500 11:01:02");
+            pw.println("200 11:02:02");
         }
 
         analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(target))){
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
             assertThat(reader.readLine(), is("10:57:01;10:59:01"));
             assertThat(reader.readLine(), is("11:01:02;11:02:02"));
         }
