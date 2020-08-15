1ebdbac060ee4fdceacd04045b1880643bce2f13
denisRudie <moonstamp1964@gmail.com>
2020-08-15 22:17:03 +0300

    5. Валидация параметров запуска. [#321407]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -10,7 +10,6 @@ import java.util.function.Predicate;
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class Search {
-    private static List<Path> pathList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         if (args.length < 2) {
@@ -23,17 +22,20 @@ public class Search {
         if (!start.toFile().isDirectory()) {
             throw new IllegalArgumentException(String.format("It is not directory: %s", start));
         }
-        search(start, args[1]).forEach(System.out::println);
+
+        search(start, p -> p.toFile().getName().endsWith(args[1]))
+                .forEach(System.out::println);
     }
 
-    public static List<Path> search(Path root, String ext) throws IOException {
-        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+    public static List<Path> search(Path root, Predicate<Path> pr) throws IOException {
+        SearchFiles searcher = new SearchFiles(pr);
         Files.walkFileTree(root, searcher);
-        return pathList;
+        return searcher.getPathList();
     }
 
     public static class SearchFiles extends SimpleFileVisitor<Path> {
         private Predicate<Path> pr;
+        private List<Path> pathList = new ArrayList<>();
 
         public SearchFiles(Predicate<Path> pr) {
             this.pr = pr;
@@ -45,6 +47,10 @@ public class Search {
                 pathList.add(file.toAbsolutePath());
             }
             return CONTINUE;
+        }
+
+        public List<Path> getPathList() {
+            return pathList;
         }
     }
 }
