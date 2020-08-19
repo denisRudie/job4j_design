115bd20aefd53f9707891c961da849d291b4de88
denisRudie <moonstamp1964@gmail.com>
2020-08-19 23:11:16 +0300

    1. Бот [#321431]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -6,9 +6,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final String HELLO = "Hello";
+    private static final String EXIT = "Exit";
+
+    public static void serverStart() {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (true) {
                 Socket socket = server.accept();
@@ -16,19 +21,47 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    StringBuilder msg = new StringBuilder();
                     boolean working = true;
+                    boolean firstString = true;
+
                     while (!(str = in.readLine()).isEmpty()) {
-                        if (str.contains("/?msg=Bye")) {
-                            working = false;
+                        if (firstString) {
+                            String req = getRequest(str);
+                            if (req.equals(HELLO)) {
+                                msg.append("Hello, my dear friend!");
+                            } else if (req.equals(EXIT)) {
+                                working = false;
+                            } else {
+                                msg.append(req);
+                            }
+                            firstString = false;
                         }
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     if (!working) {
                         break;
                     }
+                    out.write(msg.toString().getBytes());
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static String getRequest(String request) {
+        Pattern p = Pattern.compile("(/\\?msg=).+(\\sHTTP/1\\.1)");
+        Matcher m = p.matcher(request);
+        if (!m.find()) {
+            return "Bad request";
+        }
+        return request.substring(m.end(1), m.start(2));
+    }
+
+    public static void main(String[] args) {
+        serverStart();
     }
 }
