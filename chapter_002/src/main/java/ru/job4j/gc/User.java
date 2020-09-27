44c3de748a35ad36e8b9b9a77b0bc26ac7faf1ac
denisRudie <moonstamp1964@gmail.com>
2020-09-27 19:39:59 +0300

    refactor User.java for testing GC

--- chapter_002/src/main/java/ru/job4j/gc/User.java
+++ chapter_002/src/main/java/ru/job4j/gc/User.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.UUID;
 
 public class User {
+    private static int gcCounter;
     private String name;
     private Date birthday;
 
@@ -22,22 +23,20 @@ public class User {
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
-        System.out.printf("GC destroying object: %s%n", this.getName());
+        gcCounter++;
     }
 
-    public static void main(String[] args) {
-        long usedMemBefore = getUsedMemory();
-        for (int i = 0; i < 1000; i++) {
-            new User(UUID.randomUUID().toString(), new Date());
-        }
-        long usedMemAfter = getUsedMemory();
-        System.out.printf("object size: %d%n", usedMemAfter - usedMemBefore);
+    public static void main(String[] args) throws InterruptedException {
 
+        while (true) {
+            new User(UUID.randomUUID().toString(), new Date());
+            gcCounter++;
+        }
     }
 
     public static long getUsedMemory() {
-//        int kb = 1024;
+        int kb = 1024;
         Runtime runtime = Runtime.getRuntime();
-        return runtime.totalMemory() - runtime.freeMemory();
+        return (runtime.totalMemory() - runtime.freeMemory()) / kb;
     }
 }
