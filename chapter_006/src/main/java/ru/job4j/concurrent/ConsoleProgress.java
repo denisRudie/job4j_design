891abaeafe3c4cb6d0dfc7216d57047063bf3d97
denisRudie <moonstamp1964@gmail.com>
2020-10-22 01:51:11 +0300

    3. Прерывание нити [#392428]

---
+++ chapter_006/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,34 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        char[] loadingSymbols = {'\\', '|', '/'};
+        int cursor = 0;
+
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                System.out.print("\r loading ... : " + loadingSymbols[cursor++]);
+                if (cursor == loadingSymbols.length) {
+                    cursor = 0;
+                }
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+}
