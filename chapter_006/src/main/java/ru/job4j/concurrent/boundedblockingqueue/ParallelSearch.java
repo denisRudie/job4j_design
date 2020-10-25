4045624be6cbe4e881724d7a089f14f82d787354
denisRudie <moonstamp1964@gmail.com>
2020-10-26 02:28:44 +0300

    2. Обеспечить остановку потребителя. [#392418]

---
+++ chapter_006/src/main/java/ru/job4j/concurrent/boundedblockingqueue/ParallelSearch.java
@@ -0,0 +1,36 @@
+package ru.job4j.concurrent.boundedblockingqueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+
+        final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index < 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        );
+        producer.start();
+        consumer.start();
+    }
+}
